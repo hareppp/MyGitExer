4548fb2c283123594dcda486d6427141ecbcab38
Rsy <syjlzh@sina.com>
2016-09-19 00:02:09 +0800

    p2
    p2

--- src/com/atguigu/git/GitDemo.java
+++ src/com/atguigu/git/GitDemo.java
@@ -5,5 +5,6 @@ public class GitDemo {
 	public static void main(String[] args) {
 		System.out.println("hello git^^");
 		System.out.println("hello github 服务器");
+		System.out.println("update  p2");
 	}
 }
